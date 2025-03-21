ab842a6e08db7be56176bf64c57781f1852ba510
Hasan Emre Karabacak <hsn.dev@hotmail.com>
2025-03-21 16:29:12 +0300

    Initial commit of the project structure and files.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <h1 className="bg-red-500 text-white p-4 rounded-lg">Easy Recipe</h1>
+        </>
+    )
+}
+
+export default App
